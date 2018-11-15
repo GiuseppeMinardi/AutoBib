7ab79a11d88e9a906302a6306d0b1f8b7949f437
GiuseppeMinardi <38011429+GiuseppeMinardi@users.noreply.github.com>
2018-11-15 11:45:01 +0100

    Add files via upload

---
+++ AutoBib.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+#
+
+FILENAME="./Bibliography.bib"
+
+doi() {
+
+    pdftotext $1 temp.txt
+    cat temp.txt | grep doi: | cut -d: -f 2 | head -n 1 >> dois.txt
+    echo "test"
+    rm -rf temp.txt
+}
+bibmaker() {
+
+    for i in $(cat dois.txt); 
+    do 
+        curl -LH "Accept: application/x-bibtex" http://dx.doi.org/$i >> $1; 
+        echo $'\n' >> $1 
+    done
+
+}
+
+#echo "`date` User `whoami` started the script."$'\n' >> output.log
+LSTPDF=$(ls $1)
+for i in $LSTPDF; do
+    doi $1$i
+done
+
+bibmaker $FILENAME
---
+++ README.md
@@ -0,0 +1,2 @@
+# AutoBIb
+This program create a _.bib_ database from scietific papers.
