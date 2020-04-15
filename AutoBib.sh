2a70dd775bb4cab6ef2a4c08e5931ff0467d49ff
GiuseppeMinardi <giuseppe.minardi95@gmail.com>
2020-04-15 23:56:24 +0200

    new version

--- AutoBib.sh
+++ AutoBib.sh
@@ -1,28 +1,60 @@
 #! /bin/bash
 #
-
-FILENAME="./Bibliography.bib"
-
-doi() {
-
-    pdftotext $1 temp.txt -q
-    cat temp.txt | grep doi: | cut -d: -f 2 | head -n 1 >> dois.txt
-    rm -rf temp.txt
-}
-bibmaker() {
-
-    for i in $(cat dois.txt); 
-    do 
-        curl -LH "Accept: application/x-bibtex" http://dx.doi.org/$i >> $1; 
-        echo $'\n' >> $1 
-    done
-
+usage() {
+    echo "usage $0 -p <pdf file/s> [-b bibliography name]"
+    exit 1
 }
 
-echo "`date` User `whoami` started the script."$'\n' 
-LSTPDF=$(ls $1)
-for i in $LSTPDF; do
-    doi $1$i
+offline=0
+bibliography=./bibliography.bib
+while getopts ":hp:b:o" options; do              
+    case "${options}" in                         
+        h)                                      
+            usage
+            exit 1
+            ;;
+        b) bibliography=${OPTARG} ;;
+        p) 
+            articles=${OPTARG} 
+            ;;
+        o) offline=1 ;;
+        ?)                                   
+            echo "Error: -${OPTARG} requires an argument."
+            usage
+            exit 1
+            ;;
+    esac
 done
 
-bibmaker $FILENAME
+
+if [ $offline -eq 1 ]; then
+
+    for article in $articles; do
+        [ -e "$article" ] || continue
+        echo "Downloading bibliography for $article"
+        info=$(pdfinfo $article)
+        journal=$(echo "$info" | grep Subject: | sed -e 's/Subject:\(.*\),.*$/\1/' | sed -e 's/^[ \t]*//')
+        title=$(echo "$info" | sed -n 's/^Title://p' | sed -e 's/^[ \t]*//')
+        author=$(echo "$info" | sed -n 's/^Author://p' | sed -e 's/^[ \t]*//')
+        year=$(echo "$info" | awk '/CreationDate/ {print $6}' | sed -e 's/^[ \t]*//')
+
+        echo -e "@article{$(basename "article" | cut -f 1 -d '.'),
+                year = $year,
+                author = {$author},
+                title = {$title},
+                journal = {$journal}
+        }\n" >> "$bibliography"
+    done
+else
+    for article in $articles; do
+        [ -e "$article" ] || continue
+        doi=$(pdfinfo "$article" | sed -n 's/.*doi://p')
+        if [ -n '$doi' ]; then
+            echo "Downloading bibliography for $article"
+            curl -sfLH "Accept: application/x-bibtex" http://dx.doi.org/$doi >> "$bibliography"
+            echo -e "\n" >> "$bibliography"
+        else
+            echo "Cannot find doi for $article"
+        fi
+    done
+fi
--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # What is AutoBIb
 Imagine to write a long scientific article and you need to cite 100 other articles or books.
-With LaTeX is easier than any other well knows word manager, but 100 articles are 100 articles to put in your _.bib_ file. 
+With LaTeX is easier than any other program for writing documents, but 100 articles are 100 articles to put in your _.bib_ file. 
 Now imagine to take all the pdfs, extracting the **doi** and let a script download all the bib entry. This script does all of this.
 
 ## Requirement
- - _pdftotext:_ is a program that converts .pdf files to .txt files. It is necessary, but if you already hav _ranger_ as your file manager you will not need to install it.
- - _internet connection_: This program uses internet in order to retrieve entries
+The script uses `getopts` and `pdfinfo`, therefore it may cause problems. 
 
 ## How to use
 
 ```
-   ./Autobib.sh ./PdfFolder/
+   bash AutoBib.sh -p PdfFolder/*.pdf
 ```
-All pdfs must be in a single folder, switch _./PdfFolder/_ with your directory containing your articles.
+With this example all documents presents in the folder are passed to the script and all bib entries are stored in a file called _bibliography.bib_.
+You can specify the output file, if the file exist the script will append the new entries to the file, otherwise a new file will be created.
+```
+    bash AutoBib.sh -p PdfFolder/*.pdf -b mydb.bib
+```
+Here all biber entries will be stored in the file _mydb.bib_.
+
+In order to use **AutoBib** you need to be connected to internet.
+The option `-o` allows the user to run the script offline.
+**WARNING**: the offline mode create only the mandatory entries for an article (author, title, journal and year) but it can miss some parameters. 
+It is recommendeed to not use the `-o` option.
